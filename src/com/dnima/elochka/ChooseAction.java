2224b2b9723e6d297214e7f68a83093bad2598bf
Denis Medvedev <a_mdl@mail.ru>
2011-09-22 10:15:18 +0400

    - another one bites the dust. Camera NOT working...

--- src/com/dnima/elochka/ChooseAction.java
+++ src/com/dnima/elochka/ChooseAction.java
@@ -1,74 +1,73 @@
 package com.dnima.elochka;
 
 import java.io.IOException;
-import java.util.List;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Camera;
-import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Gallery;
 import android.widget.Toast;
 
-public class ChooseAction extends Activity {
+public class ChooseAction extends Activity  implements SurfaceHolder.Callback, View.OnClickListener, Camera.PictureCallback, Camera.PreviewCallback, Camera.AutoFocusCallback{
     
 	
 	public Gallery g;
 	public long pos;
-	public static Camera cam;
+	public Camera cam;
 	public DecorationFactory decorationFactory;
-	private Preview prv;
-	
-	public void addphoto( View who) {
-     // add photo to images decorations
-		try {
+	private SurfaceView sfw;
+	private SurfaceHolder sfh;
+
+	// callbacks for surface holder
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
 		cam=Camera.open();
-		Camera.Parameters cp=cam.getParameters();
-	    cp.setPictureSize(80, 80);
-	    //cam.setParameters(cp);
-		SurfaceView sfw;
-		sfw=(SurfaceView) findViewById(R.id.surfaceView1);
-		SurfaceHolder sfh=sfw.getHolder();
-		prv= new Preview(this);
-		setContentView(prv);
-		
-	
-		}
-		catch (Exception p) {
-		    Toast.makeText(ChooseAction.this, p.getLocalizedMessage(), Toast.LENGTH_SHORT).show();	
-		}
-		finally {
-			
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (cam!=null) {
+			cam.setPreviewCallback(null);
+			cam.stopPreview();
+			cam.release();
+			cam=null;
 		}
 	}
+
+	
 	public void takephoto (View who) {
 		
 	}
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
         decorationFactory=new DecorationFactory(this);
+       
          setContentView(R.layout.chooser);
+         cam=Camera.open();
+ 		sfw = (SurfaceView) findViewById(R.id.surfaceView1);
+ 		sfh = sfw.getHolder();
+ 		sfh.addCallback(this);
+ 		try {
+ 			cam.setPreviewDisplay(sfh);
+ 		} catch (IOException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
+ 		cam.startPreview();
          g = (Gallery) findViewById(R.id.gallery1);
          g.setAdapter(new ImageAdapter(this));
-
+         
          g.setOnItemClickListener(new OnItemClickListener() {
              public void onItemClick(@SuppressWarnings("rawtypes") AdapterView parent, View v, int position, long id) {
              
@@ -91,157 +90,46 @@ public class ChooseAction extends Activity {
         
              
 	 }
+	public void onAutoFocus(boolean arg0, Camera arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onPreviewFrame(byte[] arg0, Camera arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onPictureTaken(byte[] arg0, Camera arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void surfaceCreated(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		cam=Camera.open();
+		sfw = (SurfaceView) findViewById(R.id.surfaceView1);
+		sfh = sfw.getHolder();
+		sfh.addCallback(this);
+		try {
+			cam.setPreviewDisplay(sfh);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		cam.startPreview();
+	}
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void surfaceChanged(SurfaceHolder holder,int format,int width, int height) {
+		try {
+			cam.setPreviewDisplay(sfh);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
 }
-// from examples, used under Apache 2.0 license;
-class Preview extends ViewGroup implements SurfaceHolder.Callback {
-    private final String TAG = "Preview";
-
-    SurfaceView mSurfaceView;
-    SurfaceHolder mHolder;
-    Size mPreviewSize;
-    List<Size> mSupportedPreviewSizes;
-    Camera mCamera;
-
-    Preview(Context context) {
-        super(context);
-
-        mSurfaceView = new SurfaceView(context);
-        addView(mSurfaceView);
-
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
-        mHolder = mSurfaceView.getHolder();
-        mHolder.addCallback(this);
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
-
-    public void setCamera(Camera camera) {
-        mCamera = camera;
-        if (mCamera != null) {
-            mSupportedPreviewSizes = mCamera.getParameters().getSupportedPreviewSizes();
-            requestLayout();
-        }
-    }
-
-    public void switchCamera(Camera camera) {
-       setCamera(camera);
-       try {
-           camera.setPreviewDisplay(mHolder);
-       } catch (IOException exception) {
-           Log.e(TAG, "IOException caused by setPreviewDisplay()", exception);
-       }
-       Camera.Parameters parameters = camera.getParameters();
-       parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
-       requestLayout();
-
-       camera.setParameters(parameters);
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // We purposely disregard child measurements because act as a
-        // wrapper to a SurfaceView that centers the camera preview instead
-        // of stretching it.
-        final int width = resolveSize(getSuggestedMinimumWidth(), widthMeasureSpec);
-        final int height = resolveSize(getSuggestedMinimumHeight(), heightMeasureSpec);
-        setMeasuredDimension(width, height);
-
-        if (mSupportedPreviewSizes != null) {
-            mPreviewSize = getOptimalPreviewSize(mSupportedPreviewSizes, width, height);
-        }
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        if (changed && getChildCount() > 0) {
-            final View child = getChildAt(0);
-
-            final int width = r - l;
-            final int height = b - t;
-
-            int previewWidth = width;
-            int previewHeight = height;
-            if (mPreviewSize != null) {
-                previewWidth = mPreviewSize.width;
-                previewHeight = mPreviewSize.height;
-            }
-
-            // Center the child SurfaceView within the parent.
-            if (width * previewHeight > height * previewWidth) {
-                final int scaledChildWidth = previewWidth * height / previewHeight;
-                child.layout((width - scaledChildWidth) / 2, 0,
-                        (width + scaledChildWidth) / 2, height);
-            } else {
-                final int scaledChildHeight = previewHeight * width / previewWidth;
-                child.layout(0, (height - scaledChildHeight) / 2,
-                        width, (height + scaledChildHeight) / 2);
-            }
-        }
-    }
-
-    public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, acquire the camera and tell it where
-        // to draw.
-        try {
-            if (mCamera != null) {
-                mCamera.setPreviewDisplay(holder);
-            }
-        } catch (IOException exception) {
-            Log.e(TAG, "IOException caused by setPreviewDisplay()", exception);
-        }
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // Surface will be destroyed when we return, so stop the preview.
-        if (mCamera != null) {
-            mCamera.stopPreview();
-        }
-    }
-
-
-    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
-        final double ASPECT_TOLERANCE = 0.1;
-        double targetRatio = (double) w / h;
-        if (sizes == null) return null;
-
-        Size optimalSize = null;
-        double minDiff = Double.MAX_VALUE;
-
-        int targetHeight = h;
-
-        // Try to find an size match aspect ratio and size
-        for (Size size : sizes) {
-            double ratio = (double) size.width / size.height;
-            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-            if (Math.abs(size.height - targetHeight) < minDiff) {
-                optimalSize = size;
-                minDiff = Math.abs(size.height - targetHeight);
-            }
-        }
-
-        // Cannot find the one match the aspect ratio, ignore the requirement
-        if (optimalSize == null) {
-            minDiff = Double.MAX_VALUE;
-            for (Size size : sizes) {
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-        }
-        return optimalSize;
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // Now that the size is known, set up the camera parameters and begin
-        // the preview.
-        Camera.Parameters parameters = mCamera.getParameters();
-        parameters.setPreviewSize(mPreviewSize.width, mPreviewSize.height);
-        requestLayout();
-
-        mCamera.setParameters(parameters);
-        mCamera.startPreview();
-    }
-
-}
-
