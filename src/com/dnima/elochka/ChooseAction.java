de3e31e2539828778c4d65e5a14d2185b23a39e9
Denis Medvedev <a_mdl@mail.ru>
2011-10-09 23:33:56 +0400

    - intent based works, need refresh by receiving a photo

--- src/com/dnima/elochka/ChooseAction.java
+++ src/com/dnima/elochka/ChooseAction.java
@@ -1,32 +1,44 @@
 package com.dnima.elochka;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.Intent.*;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.Gallery;
+import android.widget.Toast;
 
 
 
 public class ChooseAction extends Activity  {
     
 	
+	private static final int TAKE_PICTURE = 0;
 	public Gallery g;
 	public long pos;
 	public static Camera cam;
 	public DecorationFactory decorationFactory;
+	private Uri imageUri;
 	
 	
 	
@@ -34,17 +46,63 @@ public class ChooseAction extends Activity  {
 	
 	public void takephoto (View who) {
 		decorationFactory.StoreToFiles(this);
-		Intent intent = new Intent(Intent.ACTION_DEFAULT);
-	
-	 
-	    intent.setClassName(this, com.dnima.elochka.TakePhoto.class.getName());
-	    startActivityForResult(intent,1);
+		String filename="Captured.jpg";
+		String resfname=Environment.getExternalStorageDirectory()+"/Android/data/com.dnima.elochka/files/"+filename+".jpg";
+		String mkdir1=Environment.getExternalStorageDirectory()+"/Android/data/com.dnima.elochka";
+		String mkdir2=Environment.getExternalStorageDirectory()+"/Android/data/com.dnima.elochka/files";
+			    
+		if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+			 File mkd1=new File(mkdir1);
+			 mkd1.mkdir();
+			 File mkd2=new File(mkdir2);
+			 mkd2.mkdir();
+			 // should fail or succeed, we don't care - exception will be later
+			 // but we should create the dir anyway
+		
+		} else {
+			 Toast.makeText(this, "You have no SD card - it is required", Toast.LENGTH_SHORT)
+             .show();
+			return; // just do not do anything - no card here
+		}
+			
+	    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+	    File photo = new File(resfname);
+	    intent.putExtra(MediaStore.EXTRA_OUTPUT,
+	            Uri.fromFile(photo));
+	    imageUri = Uri.fromFile(photo);
+	    startActivityForResult(intent,TAKE_PICTURE);
+	    
+	    
+	    
+	    
 	}
 	public void onActivityResult(int requestCode,int resultCode,Intent data) {
 		// went from decoration photo add
-		
-		Bundle s1=new Bundle();
-		onCreate(s1);
+		super.onActivityResult(requestCode, resultCode, data);
+	    switch (requestCode) {
+	    case TAKE_PICTURE:
+	        if (resultCode == Activity.RESULT_OK) {
+	            Uri selectedImage = imageUri;
+	            getContentResolver().notifyChange(selectedImage, null);
+	           
+	            ContentResolver cr = getContentResolver();
+	            Bitmap bitmap;
+	            try {
+	                 bitmap = android.provider.MediaStore.Images.Media
+	                 .getBitmap(cr, selectedImage);
+
+	                 decorationFactory.deco.add(new Decoration(new BitmapDrawable(bitmap)));
+	                 decorationFactory.StoreToFiles(this.getApplicationContext());
+	                
+	            } catch (Exception e) {
+	                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+	                        .show();
+	                Log.e("Camera", e.toString());
+	            }
+	        }
+	    }
+		//Bundle s1=new Bundle();
+		//onCreate(s1);
 	}
 	public static boolean isIntentAvailable(Context context, String action) {
 	    final PackageManager packageManager = context.getPackageManager();
